df433369dfe1bf6a3bf60d73b94cf572e2be0114
sandysisa <sandy.sisa@gmail.com>
2021-08-19 22:36:33 -0300

    19-08-2021

---
+++ eventdash_front/relatorios.js
@@ -0,0 +1,25 @@
+function carregaInfo(){
+    // antes de mais nada... vou verificar se tenho o objeto no LocalStorage
+    var userSTR = localStorage.getItem("userDASH"); // tento recuperar do localStorage
+    if (!userSTR) {  // se esse cara não existir
+        window.location = "index.html";
+    }
+
+    // então o usuario existe.
+
+    var user = JSON.parse(userSTR);  // como eu gravei em fomato texto, vou reconverter para objeto em memória
+
+    document.getElementById("fotoUser").innerHTML = `<img src="${user.linkFoto}"  width="100%">`;
+    document.getElementById("bioUser").innerHTML = `<h4> "${user.nome}"</h4>
+                                                    <hr>
+                                                    <b>RACF: </b>"${user.racf}"<br>
+                                                    <b>Email: </b>"${user.email}"<br>
+                                                    <b>Departamento:</b>"${user.departamento}"<br>
+                                                    <button type="button" class="btn btn-primary" onclick="logout()">Logout</button>
+    `;
+}
+
+function logout(){
+    localStorage.removeItem("userDASH");
+    window.location = "index.html"
+}
